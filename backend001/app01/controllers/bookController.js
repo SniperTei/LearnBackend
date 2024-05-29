436787c93dbed9ac4af97394da885dac4ac186f1
Sniper <sniper_zheng@163.com>
2024-05-29 16:33:55 +0800

    分页 一半

--- backend001/app01/controllers/bookController.js
+++ backend001/app01/controllers/bookController.js
@@ -1,29 +1,22 @@
 var pool = require('../config/db');
-const { generateToken, verifyToken } = require('../authorization');
+const { verifyToken } = require('../authorization');
 
 const bookListAPI = (req, res) => {
-  getBookList(req, res);
-  // pool.getConnection((err, connection) => {
-  //   if (err) {
-  //     console.error('Failed to connect to the database:', err);
-  //     return;
-  //   }
-  //   console.log('Connected to the database');
-  //   // Logic to get all books
-  //   let sql = 'SELECT * FROM tbl_books';
-  //   connection.query(sql, (error, results, fields) => {
-  //     if (error) {
-  //       throw error;
-  //     }
-  //     console.log('results:', results);
-  //     res.json(results);
-  //   });
-  //   // Release the connection
-  //   connection.release();
-  // });
+  // Check if the request has a token in the header
+  if (!req.headers.authorization) {
+    return res.json({ code: "200000", msg: "token无效" });
+  }
+  verifyToken(req, res, () => {
+    console.log('token验证通过');
+    getBookList(req, res);
+  });
 };
 
 const getBookList = (req, res) => {
+  let page = req.body.page || 1;
+  let limit = req.body.limit || 10;
+  let offset = (page - 1) * limit;
+  // Get a connection from the pool
   pool.getConnection((err, connection) => {
     if (err) {
       console.error('Failed to connect to the database:', err);
@@ -31,7 +24,9 @@ const getBookList = (req, res) => {
     }
     console.log('Connected to the database');
     // Logic to get all books
-    let sql = 'SELECT * FROM tbl_books';
+    // let sql = 'SELECT * FROM tbl_books';
+    let sql = `SELECT * FROM tbl_books LIMIT ${limit} OFFSET ${offset}`;
+    console.log('sql:', sql);
     connection.query(sql, (error, results, fields) => {
       if (error) {
         throw error;
