8c202eb14b61cb9ca72675efee832a4b245ec0a1
Sniper <sniper_zheng@163.com>
2024-11-25 23:37:13 +0800

    model

---
+++ backend001/app01/model/movieModel.js
@@ -0,0 +1,56 @@
+import mongoose from "mongoose";
+
+const MovieSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  // director: {
+  //   type: String,
+  //   required: true,
+  // },
+  actors: {
+    type: [String],
+    required: true,
+  },
+  // tags: {
+  //   type: [String],
+  //   required: true,
+  // },
+  genre: {
+    type: String,
+    required: true,
+  },
+  country: {
+    type: String,
+    required: true,
+  },
+  release_date: {
+    type: Date,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  duration: {
+    type: Number,
+    required: true,
+  },
+  jyp_viewed: {
+    type: String,
+    required: true,
+  },
+  sniper_viewed: {
+    type: String,
+    required: true,
+  },
+  // createdAt: {
+  //   type: Date,
+  //   default: Date.now,
+  // },
+});
