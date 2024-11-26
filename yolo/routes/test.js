838c5e8443e3cdfe18285ad926ac8bc2c7d8d61b
Sniper <sniper_zheng@163.com>
2024-11-26 23:40:30 +0800

    1

--- yolo/routes/test.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-// test
-router.get('/', (req, res) => {
-  res.json({ code: '666666', msg: 'test'});
-});
-
-module.exports = router;
