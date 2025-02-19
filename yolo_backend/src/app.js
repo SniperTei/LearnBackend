62e68f7509a2ec9c7aa23b9e45c5a6f06125baed
Sniper <sniper_zheng@163.com>
2025-02-19 13:26:56 +0800

    路由

--- yolo_backend/src/app.js
+++ yolo_backend/src/app.js
@@ -29,6 +29,7 @@ const fitnessRoutes = require('./routes/fitness.routes');
 const travelPlanRoutes = require('./routes/travelPlan.routes');
 const travelDiaryRoutes = require('./routes/travelDiary.routes');
 const movieRoute = require('./routes/movie.route');
+const customerRoutes = require('./routes/customer.routes');
 // const userMovieRoute = require('./routes/userMovie.route');
 
 const app = express();
@@ -77,6 +78,7 @@ app.use('/api/v1/dictionaries', dictionaryRoutes);
 app.use('/api/v1/travel-plans', travelPlanRoutes);
 app.use('/api/v1/travel-diaries', travelDiaryRoutes);
 app.use('/api/v1/movies', movieRoute);
+app.use('/api/v1/customers', customerRoutes);
 // app.use('/api/v1/user-movies', userMovieRoute);
 
 // 错误处理中间件
